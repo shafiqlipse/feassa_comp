5d45acc5628616970b81aec84f95aac1d1348bd7
shafiq <ssekittoshafiq@gmail.com>
2024-08-19 12:01:49 +0300

    kil

--- client/context_processors.py
+++ client/context_processors.py
@@ -1,11 +1,11 @@
-from football.models import Football
-from netball.models import NFixture, Netball
-from basketball3.models import Basketball3
-from basketball5.models import Basketball5
-from handball.models import Handball
-from volleyball.models import Volleyball
-from rugby7s.models import Rugby7s
-from rugby15s.models import Rugby15s
+from football.models import *
+from netball.models import *
+from basketball3.models import *
+from basketball5.models import *
+from handball.models import *
+from volleyball.models import *
+from rugby7s.models import *
+from rugby15s.models import *
 
 
 def football_tournaments(request):
@@ -37,9 +37,11 @@ def volleyball_tournaments(request):
     vtourns = Volleyball.objects.all()
     return {"vtourns": vtourns}
 
+
 def rugby7s_tournaments(request):
     r7tourns = Rugby7s.objects.all()
     return {"r7tourns": r7tourns}
+
 
 def rugby15s_tournaments(request):
     r15tourns = Rugby15s.objects.all()
@@ -51,5 +53,35 @@ from datetime import date
 
 def top_fixtures(request):
     today = date.today()
-    fixtures = NFixture.objects.filter(date=today).order_by("-date")
-    return {"top_fixtures": fixtures}
+    fot_fixtures = Fixture.objects.filter(date=today).order_by("-date")
+    return {"fot_fixtures": fot_fixtures}
+
+
+def top_nfixtures(request):
+    today = date.today()
+    net_fixtures = NFixture.objects.filter(date=today).order_by("-date")
+    return {"net_fixtures": net_fixtures}
+
+
+def top_vfixtures(request):
+    today = date.today()
+    vol_fixtures = VFixture.objects.filter(date=today).order_by("-date")
+    return {"vol_fixtures": vol_fixtures}
+
+
+def top_hfixtures(request):
+    today = date.today()
+    han_fixtures = HFixture.objects.filter(date=today).order_by("-date")
+    return {"han_fixtures": han_fixtures}
+
+
+def top_bfixtures(request):
+    today = date.today()
+    bas5_fixtures = B5Fixture.objects.filter(date=today).order_by("-date")
+    return {"bas5_fixtures": bas5_fixtures}
+
+
+def top_b3fixtures(request):
+    today = date.today()
+    b3fixtures = B3Fixture.objects.filter(date=today).order_by("-date")
+    return {"b3fixtures": b3fixtures}
