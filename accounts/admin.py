004e819a9b2487b1c36a2b3b842c0529b13414b4
shafiq <ssekittoshafiq@gmail.com>
2024-08-23 12:56:22 +0300

    official

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,6 +8,7 @@ from football.models import *
 from handball.models import *
 from volleyball.models import *
 from basketball3.models import *
+from basketball5.models import *
 from rugby7s.models import *
 from rugby15s.models import *
 from hockey.models import *
@@ -24,6 +25,7 @@ admin.site.register(Netball)
 admin.site.register(Handball)
 admin.site.register(Volleyball)
 admin.site.register(Basketball3)
+admin.site.register(Basketball5)
 admin.site.register(Rugby7s)
 admin.site.register(Rugby15s)
 admin.site.register(Hockey)
